c4ad04db47761ff9c10783b8f9806e137f211b27
k0000k <locker50@naver.com>
2024-01-29 20:33:01 +0900

    init

---
+++ src/main/java/com/server/literasea/LiteraseaApplication.java
@@ -0,0 +1,13 @@
+package com.server.literasea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraseaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraseaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/literasea/LiteraseaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.literasea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraseaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
