33b045472f2bad4b031d6a037e1359798110055e
k0000k <locker50@naver.com>
2024-02-15 23:10:17 +0900

    feat: apply login

--- src/main/java/com/server/literasea/controller/TodayController.java
+++ src/main/java/com/server/literasea/controller/TodayController.java
@@ -2,9 +2,11 @@ package com.server.literasea.controller;
 
 import com.server.literasea.dto.BaseResponse;
 import com.server.literasea.dto.TodayArticleDto;
+import com.server.literasea.entity.Users;
 import com.server.literasea.service.TodayService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +22,14 @@ public class TodayController {
 
     @GetMapping("/")
     @Operation(summary = "오늘 요약 할 지문 5개 불러오기", description = "로그인은 추후 추가 예정입니다.")
-    public BaseResponse<List<TodayArticleDto>> todayGet() {
-        List<TodayArticleDto> todayArticleDtos = todayService.findTodayArticles();
+    public BaseResponse<List<TodayArticleDto>> todayGet(@AuthenticationPrincipal Users user) {
+        List<TodayArticleDto> todayArticleDtos = todayService.findTodayArticles(user);
         return BaseResponse.success("ok", todayArticleDtos);
     }
 
     @PostMapping("/post")
     @Operation(summary = "유저가 작성한 요약 전송하고 결과 불러오기", description = "AI 정해지면 개발 예정입니다.")
-    public BaseResponse<String> todayPost() {
+    public BaseResponse<String> todayPost(@AuthenticationPrincipal Users user) {
         // 미구현 상태입니다.
         return BaseResponse.success("ok", "Hello, world!");
     }
